f561bff44826d193809db25fae733948ee3e40bf
Lev Livshits <livshitz.lewa@yandex.ru>
2021-09-27 20:29:45 +0300

    more tests

--- HomeTask2/InterTask.Tests/LazyFactoryTests.cs
+++ HomeTask2/InterTask.Tests/LazyFactoryTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System;
 using InterTask;
+using System.Threading;
 
 namespace InterTask.Tests
 {
@@ -30,6 +31,34 @@ namespace InterTask.Tests
             Func<int> func = () => random.Next();
             var lazy = LazyFactory.CreateOneThreadLazy<int>(func);
             Assert.AreEqual(lazy.Get(), lazy.Get());
+        }
+
+        [Test]
+        public void FirstTry()
+        {
+            Func<int> func = () => 5;
+            var lazy = LazyFactory.CreateOneThreadLazy<int>(func);
+            var numberOfThreads = Environment.ProcessorCount;
+            var threads = new Thread[numberOfThreads];
+            var numbers = new int[numberOfThreads];
+            int varNumber = 3;
+            for (int i = 0; i < numberOfThreads; ++i)
+            {
+                threads[i] = new Thread(() => varNumber = lazy.Get());
+                threads[i].Start();
+            }
+            
+            for (int i = 0; i < numberOfThreads; ++i)
+            {
+                threads[i].Join();
+            }
+            Console.WriteLine(numberOfThreads);
+            for (int i = 0; i < numberOfThreads; ++i)
+            {
+                Console.WriteLine($"number = {varNumber}");
+                //Assert.AreEqual(1, 5);
+            }
+            Assert.AreEqual(1, 5);
         }
     }
 }
