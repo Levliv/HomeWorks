0f39162c61efe172ce20f44720a79964cff355b6
Lev Livshits <livshitz.lewa@yandex.ru>
2021-10-24 16:23:16 +0300

    -Iserver

--- HomeTask4/MyFTP/Program.cs
+++ HomeTask4/MyFTP/Program.cs
@@ -17,12 +17,12 @@ namespace MyFTP
             //taskB.Start();
             //taskA.Wait();
             //taskB.Wait();
-            string path = "1 ./Tests/Files/testfile.txt";
+            //string path = "1 ./Tests/Files/testfile.txt";
             var server = new Server();
             //var str = server.List(path);
-            var (res, bytes) =  server.Get(path);
-            Console.WriteLine(res);
-            Console.WriteLine(Encoding.UTF8.GetString(bytes));
+            //var (res, bytes) =  server.Get(path);
+            //Console.WriteLine(res);
+            //Console.WriteLine(Encoding.UTF8.GetString(bytes));
         }
     }
 }
