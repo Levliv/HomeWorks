1ff9566d7652165b1c9b18338e9998f04845911e
Lev Livshits <livshitz.lewa@yandex.ru>
2021-11-07 15:56:53 +0300

    init

---
+++ HomeTask4/MyFTPServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFTPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
