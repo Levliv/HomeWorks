822da227c8083bf89c01162a28591e230b2b6a71
Lev Livshits <livshitz.lewa@yandex.ru>
2021-12-04 22:36:05 +0300

    Input data processing transfered into main

--- MyFTP/MyFTPClient/Client.cs
+++ MyFTP/MyFTPClient/Client.cs
@@ -1,27 +1,13 @@
 ﻿using System.Net.Sockets;
-using System.Text;
 
 namespace MyFTPClient;
 
-/// <summary>
-/// Get and List Requests Enumeration
-/// </summary>
-public enum RequestType
-{
-    Get,
-    List
-}
 
 /// <summary>
 /// Class for Client of TCP protocol
 /// </summary>
 public class Client
 {
-    /// <summary>
-    /// Type of request from the enum: Get or List
-    /// </summary>
-    public RequestType FtpRequestType { get; private set; }
-
     /// <summary>
     /// Ip to listen
     /// </summary>
@@ -48,68 +34,13 @@ public class Client
     public NetworkStream? MyStreamReader { get; private set; }
 
     /// <summary>
-    /// Structs to preserve information about file got from Server by Get Request
-    /// </summary>
-    private GetResponseStruct GetResponse;
-
-    /// <summary>
     /// Constructor for the Client, creating new TCP client and connecting to the server
     /// </summary>
     public Client(string ipString, int port)
     {
         TcpClient = new TcpClient();
-        //TcpClient = new TcpClient(ipString, port);
         IpString = ipString;
         Port = port;
-    }
-
-    /// <summary>
-    /// Sending requests to Server by invoking Get/List Client Methods
-    /// </summary>
-    public void ClientRequest(string requestString)
-    {
-        var splitеedRequestString = requestString.Split(" ");
-        var request = splitеedRequestString[0];
-        var path = splitеedRequestString[1];
-        if (request == "2")
-        {
-            FtpRequestType = RequestType.Get;
-        }
-        else if (request == "1")
-        {
-            FtpRequestType = RequestType.List;
-        }
-        switch (FtpRequestType)
-        {
-            case RequestType.Get:
-                GetResponse = Get(path);
-                break;
-            case RequestType.List:
-                ResultsOfListResponse = List(path);
-                break;
-        }
-    }
-
-    /// <summary>
-    /// Printing the results got by TCP
-    /// </summary>
-    public void PrintResults()
-    {
-        switch (FtpRequestType)
-        {
-            case RequestType.Get:
-                Console.WriteLine(Encoding.UTF8.GetString(GetResponse.Data));
-                break;
-            case RequestType.List:
-                Console.Write(ResultsOfListResponse.Count() + " ");
-                foreach (var item in ResultsOfListResponse)
-                {
-                    Console.WriteLine(item.Name + " " + item.IsDir + " ");
-                }
-                break;
-            default:
-                break;
-        }
     }
 
     /// <summary>
--- MyFTP/MyFTPClient/Program.cs
+++ MyFTP/MyFTPClient/Program.cs
@@ -1,24 +1,38 @@
 ﻿using System.Net;
+using System.Text;
+
 namespace MyFTPClient;
 
 static class Program
 {
     static void Main(string[] args)
     {
-        if (int.TryParse(args[1], out int port) && IPAddress.TryParse(args[0], out IPAddress? ip))
+        if (int.TryParse(args[1], out int port) && IPAddress.TryParse(args[0], out IPAddress? ip) && int.TryParse(args[2], out int requestCode))
         {
             string ipString = args[0];
             Console.WriteLine($"port {port} and ip {ip} recognised successfully");
             while (true)
             {
-                string? request = args[2];
-                if (request == null || ip == null)
+                string? path = args[3];
+                if (path == null || ip == null)
                 {
-                    throw new ArgumentNullException("Request and ip should not be NULL");
+                    throw new ArgumentNullException("Path should not be NULL");
                 }
                 var client = new Client(ipString, port);
-                client.ClientRequest(request);
-                client.PrintResults();
+                if (requestCode == 2)
+                {
+                    var GetResponse = client.Get(path);
+                    Console.WriteLine(Encoding.UTF8.GetString(GetResponse.Data));
+                }
+                else if (requestCode == 1)
+                {
+                    var ResultsOfListResponse = client.List(path);
+                    Console.Write(ResultsOfListResponse.Count() + " ");
+                    foreach (var item in ResultsOfListResponse)
+                    {
+                        Console.WriteLine(item.Name + " " + item.IsDir + " ");
+                    }
+                }
             }
         }
         else
