4c511cd97738e69fe3eaa4360849ac11729cdab0
Lev Livshits <livshitz.lewa@yandex.ru>
2021-12-07 16:51:08 +0300

    no tetst

---
+++ MDA5/MDA5/HashCounter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Collections.Concurrent;
+
+namespace MyMD5
+{
+    internal static class HashCounter
+    {
+        public static byte[] ComputeHashSingleThread(string path)
+        {
+            return ComputeDirectoryHash(path);
+        }
+        public static byte[] ComputeHashMultiThread(string path)
+        {
+            return ComputeDirectoryHash(path);
+
+        }
+        private static byte[] ComputeDirectoryHashParallel(string path)
+        {
+            var dataDir = new BlockingCollection<byte[]>();
+            var md5Hasher = MD5.Create();
+            var directories = Directory.EnumerateDirectories(path).OrderBy(x => x);
+            var dirName = path.Split('\\').Last();
+            var totalByteOfHashes = md5Hasher.ComputeHash(Encoding.UTF8.GetBytes(dirName));
+            var tasks = new List<Task>();
+            directories.AsParallel().ForAll(x => dataDir.Add(ComputeDirectoryHashParallel(x)));
+            foreach (var dataHash in dataDir)
+            {
+                totalByteOfHashes = totalByteOfHashes.Concat(dataHash).ToArray();
+            }
+            var dataFile = new BlockingCollection<byte[]>();
+            var files = Directory.EnumerateFiles(path).OrderBy(x => x);
+            files.AsParallel().ForAll(x => dataFile.Add(ComputeFileHash(x)));
+            foreach (var fileHash in dataFile)
+            {
+                totalByteOfHashes = totalByteOfHashes.Concat(fileHash).ToArray();
+            }
+            return md5Hasher.ComputeHash(totalByteOfHashes);
+        }
+
+        private static byte[] ComputeDirectoryHash(string path)
+        {
+            var md5Hasher = MD5.Create();
+            var directories = Directory.EnumerateDirectories(path).OrderBy(x => x);
+            var dirName = path.Split('\\').Last();
+            var totalByteOfHashes = md5Hasher.ComputeHash(Encoding.UTF8.GetBytes(dirName));
+            foreach (var directory in directories)
+            {
+                var subDirHash = ComputeDirectoryHash(directory);
+                totalByteOfHashes = totalByteOfHashes.Concat(subDirHash).ToArray();
+            }
+            var files = Directory.EnumerateFiles(path).OrderBy(x => x);
+            foreach (var file in files)
+            {
+                var fileHash = ComputeFileHash(file);
+                totalByteOfHashes = totalByteOfHashes.Concat(fileHash).ToArray();
+            }
+            return md5Hasher.ComputeHash(totalByteOfHashes);
+        }
+
+        private static byte[] ComputeFileHash(string path)
+        {
+            using var md5Hasher = MD5.Create();
+            using var fileStream = File.OpenRead(path);
+            var fileHashBytes = md5Hasher.ComputeHash(fileStream);
+            return fileHashBytes;
+        }
+    }
+}
---
+++ MDA5/MDA5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+namespace MyMD5;
+
+class Programm
+{
+    private static (long, double) MethodRunner(Func<byte[]> myMethod)
+    {
+        var cyrcles = 10;
+        var stopWatch = Stopwatch.StartNew();
+        var times = new List<long>();
+        for (int i = 0; i < cyrcles; i++)
+        {
+            stopWatch.Restart();
+            myMethod();
+            stopWatch.Stop();
+            times.Add(stopWatch.ElapsedMilliseconds);
+        }
+        var sum = times.Sum();
+        var avg = sum / cyrcles;
+        long deviation = 0;
+        foreach (var timeInstance in times)
+        {
+            deviation += (timeInstance - avg) * (timeInstance - avg);
+        }
+        deviation /= cyrcles;
+        var standardDeviationQuadratic = Math.Sqrt(deviation);
+        return (avg, standardDeviationQuadratic);
+    }
+    private static void Timer()
+    {
+        var path = "..\\..\\..\\..\\Tests";
+        for (var i = 0; i < 10; ++i)
+        {
+            var (singleThreadTime, singleThreadSQD) = MethodRunner(() => HashCounter.ComputeHashSingleThread(path));
+            var (MultiThreadTime, MultiThreadSQD) = MethodRunner(() => HashCounter.ComputeHashMultiThread(path));
+            Console.WriteLine("Single Thread: {0}ms +- {1}ms", singleThreadTime, singleThreadSQD);
+            Console.WriteLine("Multi Thread: {0}ms +- {1}ms", MultiThreadTime, MultiThreadSQD);
+        }
+
+    }
+    public static void Main()
+    {
+        Timer();
+    }
+}
