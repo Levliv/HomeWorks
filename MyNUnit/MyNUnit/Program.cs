dabf268f85b98f678b6ccc1cd14e1ee139340833
Lev Livshits <livshitz.lewa@yandex.ru>
2022-04-10 21:26:00 +0300

    program fixed

--- MyNUnit/MyNUnit/Program.cs
+++ MyNUnit/MyNUnit/Program.cs
@@ -1,35 +1,29 @@
-﻿namespace MyNUnit;
-
+﻿using MyNUnit;
 using System;
 using System.IO;
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        if (args.Length == 0)
-        {
-            Console.WriteLine("Not enought args has been entered, need: 1");
-            return;
-        }
 
-        var path = args[0];
-        try
-        {
-            TestRunner.Start(path);
-            TestRunner.PrintTestResults();
-        }
-        catch (DirectoryNotFoundException)
-        {
-            Console.WriteLine($"Path: {path} is invaild, couldn't file the file");
-        }
-        catch (FieldAccessException)
-        {
-            Console.WriteLine($"You have no access to this file");
-        }
-        catch (FileLoadException)
-        {
-            Console.WriteLine("An error ocures while dll files loading");
-        }
-    }
+if (args.Length != 1)
+{
+    Console.WriteLine("Wrong number of args has been entered, need: 1");
+    return;
+}
+
+var path = args[0];
+try
+{
+    TestRunner.Start(path);
+    TestRunner.PrintTestResults();
+}
+catch (DirectoryNotFoundException)
+{
+    Console.WriteLine($"Path: {path} is invaild, couldn't file the file");
+}
+catch (FieldAccessException)
+{
+    Console.WriteLine($"You have no access to this file");
+}
+catch (FileLoadException)
+{
+    Console.WriteLine("An error ocures while dll files loading");
 }
